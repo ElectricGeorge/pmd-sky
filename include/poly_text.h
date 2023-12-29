c44e818bef0e086c3dc14d18a0ae280138a2d3b4
ElectricGeorge <17412569+ElectricGeorge@users.noreply.github.com>
2023-12-29 18:59:09 -0500

    Decomp ov29_022DC2B8

--- include/poly_text.h
+++ include/poly_text.h
@@ -2,22 +2,21 @@
 #define PMDSKY_OVERLAY_29_022DC240_H
 
 struct unk_ov29_022DC240_t {
-    char unk0[0x1962a];
-
-    struct unk_ov29_022DC240_1962a_t {
-        short unk0;
-        short unk2;
-        short unk4;
-        short unk6;
-        short unk8;
-        short unka;
-        short unkc;
-        short unke;
-    } unk1962a;
-
+    char unk0[0x17704];
+    short unk17704[0xf92];
+    short unk19628;
+    short unk1962a;
+    short unk1962c;
+    short unk1962e;
+    short unk19630;
+    short unk19632;
+    short unk19634;
+    short unk19636;
+    short unk19638;
     short unk1963a;
 };
 
 void ov29_022DC240(struct unk_ov29_022DC240_t *param0);
+void ov29_022DC2B8(struct unk_ov29_022DC240_t *param0);
 
 #endif //PMDSKY_OVERLAY_29_022DC240_H
--- src/poly_text.c
+++ src/poly_text.c
@@ -5,12 +5,21 @@ extern void MemZero(void* ptr, u32 len);
 void ov29_022DC240(struct unk_ov29_022DC240_t *param0)
 {
     MemZero(param0, sizeof(struct unk_ov29_022DC240_t));
-    param0->unk1962a.unk0 = 0;
-    param0->unk1962a.unk2 = 0;
-    param0->unk1962a.unk4 = 0x100;
-    param0->unk1962a.unk6 = 0xc0;
-    param0->unk1962a.unk8 = 0x40;
-    param0->unk1962a.unka = 0x18;
-    param0->unk1962a.unkc = 0x4;
-    param0->unk1962a.unke = 0x8;
+    param0->unk1962a = 0;
+    param0->unk1962c = 0;
+    param0->unk1962e = 0x100;
+    param0->unk19630 = 0xc0;
+    param0->unk19632 = 0x40;
+    param0->unk19634 = 0x18;
+    param0->unk19636 = 0x4;
+    param0->unk19638 = 0x8;
+}
+
+void ov29_022DC2B8(struct unk_ov29_022DC240_t *param0)
+{
+    param0->unk19628 = 0;
+    int v1 = (short)(param0->unk19632 * param0->unk19634);
+    for(int i = 0; i < v1; i = (short)(i + 1)) {
+        (param0->unk17704)[i] = 0;
+    }
 }
